070879aee2f894d21378a93e58fe72924eccfcf1
Sachin <Sac@abcd.com>
2023-11-17 12:39:11 +0530

    Adding new File from IntelliJ

---
+++ src/main/java/Deutsche/Random_Number.java
@@ -0,0 +1,11 @@
+package Deutsche;
+
+public class Random_Number {
+    public static void main(String[] args) {
+// Generating random numbers
+        System.out.println("1st Random Number: " + Math.random());
+        System.out.println("2nd Random Number: " + Math.random());
+        System.out.println("3rd Random Number: " + Math.random());
+        System.out.println("4th Random Number: " + Math.random());
+    }
+}
