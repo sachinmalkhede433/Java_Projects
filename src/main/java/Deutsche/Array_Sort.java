cce067bc5670d0b24642bdd186fe8bf165dff86c
sachinmalkhede433 <62205817+sachinmalkhede433@users.noreply.github.com>
2024-05-19 22:30:57 +0530

    Update Array_Sort.java

--- src/main/java/Deutsche/Array_Sort.java
+++ src/main/java/Deutsche/Array_Sort.java
@@ -7,7 +7,7 @@ public class Array_Sort {
     {
 //defining an array of integer type
         int [] array = new int [] {90, 23, 5, 109, 12, 22, 67, 34};
-//invoking sort() method of the Arrays class
+//invoking sort() method of the Arrays class  
         Arrays.sort(array);
         System.out.println("Elements of array sorted in ascending order: ");
 //prints array using the for loop
